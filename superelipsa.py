63e1871818797c30b51ceea362404eafd2e4dc88
B4rtekk1 <113917881+B4rtekk1@users.noreply.github.com>
2023-12-19 16:56:27 +0100

    Add files via upload

---
+++ superelipsa.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def superelipsa(a, b, m, n, nPoints=1000):
+    t = np.linspace(0, 2 * np.pi, nPoints)
+    x = a * np.sign(np.cos(t)) * np.abs(np.cos(t))**(2/m)
+    y = b * np.sign(np.sin(t)) * np.abs(np.sin(t))**(2/n)
+    return x, y
+
+def plot_superelipsa(a, b, m, n):
+    x, y = superelipsa(a, b, m, n)
+    plt.plot(x, y)
+    plt.title('Superelipsa')
+    plt.xlabel('Oś x')
+    plt.ylabel('Oś y')
+    plt.grid(True)
+    plt.axis('equal')
+    plt.show()
+
+# Przykładowe parametry: a=1, b=1, m=2, n=2
+plot_superelipsa(1, 1, 2, 0.5)
